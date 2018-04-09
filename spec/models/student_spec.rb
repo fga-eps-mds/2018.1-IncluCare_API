58c9156631f8533aeeb40e416ec83626ddb7109e
William Almeida <will.allmeida@gmail.com>
2018-04-09 19:58:59 -0300

    Adding coveralls configs

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 describe Student do
-  it "é válido quando nome, último nome e email estão presentes" do
-  user = Student.new( name: 'Bruce')
+  it "should have a name" do
+  student = Student.new( name: 'Bruce')
 
-   expect(user).to be_valid
+   expect(student).to be_valid
   end
-  it do
+  it "shouldn't have a name" do
   student = Student.new(name: nil)
   student.valid?
   expect(student.errors[:name]).to include("can't be blank")
