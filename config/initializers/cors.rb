fc4d60185bfe9a38dd85d696e107850abff46026
William Almeida <will.allmeida@gmail.com>
2018-04-12 19:25:50 -0300

    Creating Student Class

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,12 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-Rails.application.config.middleware.use, Rack::Cors do
- allow do
-   origins '*'
-   resource '*',
-     :headers: :any,
-     :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
-     :methods: [:get, :post, :put, :patch, :delete, :options, :head]
+Rails.application.config.middleware.use Rack::Cors do
+  allow do
+    origins '*'
+    resource '*',
+    :headers => :any,
+    :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+    :methods => [:get, :post, :options, :delete, :put]
   end
 end
