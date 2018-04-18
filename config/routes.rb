042ff96c8550d51c11e192e349192daa29a8306a
byronkamal <byronkamal@hotmail.com>
2018-04-17 23:56:15 -0300

    Deleting comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   resources :team_members
   resources :students
   mount_devise_token_auth_for 'TeamMember', at: 'auth'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
