133f90d74e80ad89f68f1907d4432595ac0655f3
William Almeida <will.allmeida@gmail.com>
2018-05-05 01:35:43 -0300

    Adding student presence validation
    Co-authored-by: Byron Kamal <byronkamal@hotmail.com>
    Co-authored-by: Gabriel ALves <gabrielsoares.prog@gmail.com>

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,14 @@
 class Student < ApplicationRecord
   validates :name, presence: true
+  validates :birth_date, presence: true
+  validates :age, presence: true
+  validates :nationality, presence: true
+  validates :year, presence: true
+  validates :student_class, presence: true
+  validates :shift, presence: true
+  validates :father_name, presence: true
+  validates :mother_name, presence: true
+  validates :address, presence: true
+  validates :parent_telephone, presence: true
+  validates :parent_email, presence: true
 end
