db93f4c4f35037cb0bf29203d04bf4601bd956bf
Gabriel Alves <gabrielsoares.prog@gmail.com>
2018-05-10 23:08:48 -0300

    Add info of teacher.
    Co-authored-by: jpmota2208 <jpmota.unb@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180417212341) do
+ActiveRecord::Schema.define(version: 20180511020236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20180417212341) do
     t.string "parent_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "teacher_name"
   end
 
   create_table "team_members", force: :cascade do |t|
