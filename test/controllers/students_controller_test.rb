522dc8da4420e587891abe75c1b4a48f0befe37e
Gabriel Alves <gabrielsoares.prog@gmail.com>
2018-05-14 23:30:49 -0300

    update test of student

--- test/controllers/students_controller_test.rb
+++ test/controllers/students_controller_test.rb
@@ -13,7 +13,7 @@ class StudentsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create student" do
     assert_difference('Student.count') do
-      post students_url, params: { student: { address: @student.address, age: @student.age, birth_date: @student.birth_date, father_name: @student.father_name, mother_name: @student.mother_name, name: @student.name, nationality: @student.nationality, parent_email: @student.parent_email, parent_telephone: @student.parent_telephone, shift: @student.shift, student_class: @student.student_class, year: @student.year } }, as: :json
+      post students_url, params: { student: { address: @student.address, age: @student.age, birth_date: @student.birth_date, father_name: @student.father_name, mother_name: @student.mother_name, name: @student.name, nationality: @student.nationality, parent_email: @student.parent_email, parent_telephone: @student.parent_telephone, shift: @student.shift, student_class: @student.student_class, year: @student.year, teacher_name: @student.teacher_name } }, as: :json
     end
 
     assert_response 201
@@ -21,7 +21,7 @@ class StudentsControllerTest < ActionDispatch::IntegrationTest
 
   test "shouldn't create student" do
     assert_no_difference('Student.count') do
-      post students_url, params: { student: { address: @student.address, age: @student.age, birth_date: @student.birth_date, father_name: @student.father_name, mother_name: @student.mother_name, name: @student2.name, nationality: @student.nationality, parent_email: @student.parent_email, parent_telephone: @student.parent_telephone, shift: @student.shift, student_class: @student.student_class, year: @student.year } }, as: :json
+      post students_url, params: { student: { address: @student.address, age: @student.age, birth_date: @student.birth_date, father_name: @student.father_name, mother_name: @student.mother_name, name: @student2.name, nationality: @student.nationality, parent_email: @student.parent_email, parent_telephone: @student.parent_telephone, shift: @student.shift, student_class: @student.student_class, year: @student.year, teacher_name: @student.teacher_name } }, as: :json
     end
 
     assert_response :unprocessable_entity
@@ -33,12 +33,12 @@ class StudentsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update student" do
-    patch student_url(@student), params: { student: { address: @student.address, age: @student.age, birth_date: @student.birth_date, father_name: @student.father_name, mother_name: @student.mother_name, name: @student.name, nationality: @student.nationality, parent_email: @student.parent_email, parent_telephone: @student.parent_telephone, shift: @student.shift, student_class: @student.student_class, year: @student.year } }, as: :json
+    patch student_url(@student), params: { student: { address: @student.address, age: @student.age, birth_date: @student.birth_date, father_name: @student.father_name, mother_name: @student.mother_name, name: @student.name, nationality: @student.nationality, parent_email: @student.parent_email, parent_telephone: @student.parent_telephone, shift: @student.shift, student_class: @student.student_class, year: @student.year, teacher_name: @student.teacher_name } }, as: :json
     assert_response 200
   end
 
   test "shouldn't update student" do
-    patch student_url(@student2), params: { student: { address: @student2.address, age: @student2.age, birth_date: @student2.birth_date, father_name: @student2.father_name, mother_name: @student2.mother_name, name: @student2.name, nationality: @student2.nationality, parent_email: @student2.parent_email, parent_telephone: @student2.parent_telephone, shift: @student2.shift, student_class: @student2.student_class, year: @student2.year } }, as: :json
+    patch student_url(@student2), params: { student: { address: @student2.address, age: @student2.age, birth_date: @student2.birth_date, father_name: @student2.father_name, mother_name: @student2.mother_name, name: @student2.name, nationality: @student2.nationality, parent_email: @student2.parent_email, parent_telephone: @student2.parent_telephone, shift: @student2.shift, student_class: @student2.student_class, year: @student2.year, teacher_name: @student2.teacher_name } }, as: :json
     assert_response :unprocessable_entity
   end
 
