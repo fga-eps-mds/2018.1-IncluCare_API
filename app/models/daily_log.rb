57ab33e18eac7c350bc4783aae4db25e643b9e39
IgorVeludo <velvet.guimaraes@gmail.com>
2018-05-16 20:57:56 -0300

    Adding DailyLog presence validation

--- app/models/daily_log.rb
+++ app/models/daily_log.rb
@@ -1,3 +1,9 @@
 class DailyLog < ApplicationRecord
   belongs_to :student
+
+  validates_presence_of :note, presence: true, message: 'can t be left blank'
+  validates_presence_of :activity, presence: true, message: 'can t be left blank'
+  validates_presence_of :date, presence: true, message: 'can t be left blank'
+  validates_presence_of :student_id, presence: true, message: 'can t be left blank'
+
 end
