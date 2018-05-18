00992c70f99936fbc24a28ad08d85bc9a9b7e3a9
William Almeida <will.allmeida@gmail.com>
2018-05-18 15:53:29 -0300

    AddingDailyLog seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,3 +30,7 @@ puts "TeamMembers seeded."
   )
 end
 puts "Students seeded."
+
+DailyLog.create!(date: "10/10/2010", activity: 'pescar', note: 'A', student_id: 1)
+DailyLog.create!(date: "10/10/2010", activity: 'nadar', note: 'B', student_id: 2)
+puts "DailyLogs seeded."
