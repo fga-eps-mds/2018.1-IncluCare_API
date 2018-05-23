d58623cfd665fa9abccea24aad7fed98348a0080
William Almeida <will.allmeida@gmail.com>
2018-05-23 19:29:32 -0300

    Validating Report attributes

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,11 @@
 class Report < ApplicationRecord
   belongs_to :student
 
+  validates_presence_of :reason, presence: true, message: 'can t be left blank'
+  validates_presence_of :general_information, presence: true, message: 'can t be left blank'
+  validates_presence_of :synthesis, presence: true, message: 'can t be left blank'
+  validates_presence_of :conclusion, presence: true, message: 'can t be left blank'
+  validates_presence_of :family_orientation, presence: true, message: 'can t be left blank'
+  validates_presence_of :school_orientation, presence: true, message: 'can t be left blank'
+  
 end
