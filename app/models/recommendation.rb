62ae3bd00c0e67f50ad340f032e8b69c1e12c463
byronkamal <byronkamal@hotmail.com>
2018-06-07 21:42:31 -0300

    Validating Recommendations of Students
    Co-authored-by: marcelo046 <marc3lo046@gmail.com>

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -1,3 +1,7 @@
 class Recommendation < ApplicationRecord
   belongs_to :student
+
+  validates_presence_of :student_id, presence: true, message: 'cannot be left blank'
+  validates_presence_of :title, presence: true, message: 'cannot be left blank'
+  validates_presence_of :body, presence: true, message: 'cannot be left blank'
 end
